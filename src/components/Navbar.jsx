3cb423644daf14e1c1fec4120a917861af793449
Sujal Dubey <sujaldubey2636@gmail.com>
2026-09-19 10:26:09 +0530

    UI: Hide Get in touch button on mobile in Navbar to fix layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -108,7 +108,7 @@ export default function Navbar() {
           <div className="flex justify-end items-center gap-4">
             <a
               onClick={() => handleNav('contact-section')}
-              className="btn-primary text-sm whitespace-nowrap cursor-pointer select-none hidden sm:inline-flex"
+              className="btn-primary text-sm whitespace-nowrap cursor-pointer select-none hidden md:inline-flex"
             >
               Get in touch
             </a>
